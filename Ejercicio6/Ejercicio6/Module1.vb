f2f82964d6501766abdcdd04b8d76ffef465f687
HugoPexegueiro <113344246+HugoPexegueiro@users.noreply.github.com>
2022-10-04 22:31:27 +0200

    Finalizado

---
+++ Ejercicio6/Ejercicio6/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, c, d, menor As Integer
+        a = Convert.ToInt32(InputBox("Introduzca el primer numero"))
+        b = Convert.ToInt32(InputBox("Introduzca el segundo numero"))
+        c = Convert.ToInt32(InputBox("Introduzca el tercer numero"))
+        d = Convert.ToInt32(InputBox("Introduzca el cuarto numero"))
+        If a > b Then
+            menor = b
+        Else
+            menor = a
+        End If
+        If menor > c Then
+            menor = c
+            If menor > d Then
+                menor = d
+                Console.WriteLine("El numero menor es " & menor)
+            Else
+                Console.WriteLine("El numero menor es " & menor)
+            End If
+        Else
+            If menor > d Then
+                menor = d
+                Console.WriteLine("El numero menor es " & menor)
+            Else
+                Console.WriteLine("El numero menor es " & menor)
+            End If
+        End If
+        Console.ReadLine()
+    End Sub
+
+End Module
